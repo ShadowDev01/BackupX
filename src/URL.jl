7d8a8668ee86794272adc1b9728a47a9ec3a9857
miti <mitikoman494@gmail.com>
2023-06-20 15:15:18 +0330

    some changes in url structure

--- src/URL.jl
+++ src/URL.jl
@@ -14,23 +14,37 @@ struct URL
     fragment::String
 end
 
-function URL(url::String)
+function extract(host)
+    tlds = Set()
+    for line in eachline("tlds.txt")
+        occursin(Regex("\\b$line\\b"), host) && push!(tlds, line)
+    end
+    tld = collect(tlds)[findmax(length, collect(tlds))[2]]
+    rest = rsplit(replace(host, tld => ""), ".", limit=2)
+    if length(rest) > 1
+        subdomain, domain = rest
+    else
+        subdomain = ""
+        domain = rest[1]
+    end
+    return (subdomain, domain, strip(tld, '.'))
+end
+
+function URL(url::AbstractString)
     url::String = replace(url, "www." => "")
-    parts = match(r"^(\w+):\/\/(([\w\-]+):(.+)\@)?(([\w\-]+\.)?([\w\-]+)\.([\w\-]+)):?(\d+)?([\/,\w\.]+)?(\?[^\#]*)?(\#.*)?", url).captures
+    parts = match(r"^(\w+):\/\/(([\w\-]+):(.+)\@)?([\w\-\.]+):?(\d+)?([\/,\w\.]+)?(\?[^\#]*)?(\#.*)?", url).captures
     replace!(parts, nothing => "")
-    deleteat!(parts,2)
+    deleteat!(parts, 2)
     scheme::String = parts[1]
     username::String = parts[2]
     password::String = parts[3]
     host::String = parts[4]
-    subdomain::String = strip(parts[5], '.')
-    domain::String = strip(parts[6], '.')
-    tld::String = strip(parts[7], '.')
-    port::String = parts[8]
-    path::String = parts[9]
+    subdomain::String, domain::String, tld::String = extract(host)
+    port::String = parts[5]
+    path::String = parts[6]
     dirctory::String = dirname(path)
     file::String = basename(path)
-    query::String = parts[10]
-    fragment::String  = parts[11]
+    query::String = parts[7]
+    fragment::String  = parts[8]
     return URL(scheme, username, password, host, subdomain, domain, tld, port, path, dirctory, file, query, fragment)
- end
+end
